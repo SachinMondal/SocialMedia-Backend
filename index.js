7f13e66ff1d226f82007f2bc10f64eb6d7cac798
Sachin <sachinmondal720@gmail.com>
2024-02-09 21:57:23 +0530

    re-dd

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const morgan = require("morgan");
 dotenv.config();
 
 // Resolve the absolute path of the current directory
-const __dirname__ = path.resolve();
+
 
 // Set up the static files directory
-const viewsDirectoryPath = path.join(__dirname__, 'views/build');
+const viewsDirectoryPath = path.join(__dirname, 'views/build');
 app.use(express.static(viewsDirectoryPath));
 
 // Connect to the database
